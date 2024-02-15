f2e54f989c2197ef8e497f56979d7a4060b4fb53
Zigarov <jluk.376@gmail.com>
2024-02-15 12:40:23 +0100

    Init 1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.scss'
+import mainExample01 from './example01'
+import mainExample02 from './example02'
+import mainExample03 from './example03'
+
+mainExample01()
+// mainExample02()
+// mainExample03()
