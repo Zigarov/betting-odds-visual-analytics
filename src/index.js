09fad2b72fa1cc598004dd7cbe030586dfe77a58
Zigarov <jluk.376@gmail.com>
2024-02-16 11:32:48 +0100

    init 1.0
    Inizializzazione del progetto senza utilizzare webpack

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
 // index.js
 
-// Importa D3.js e la funzione per creare il Parallel Coordinates Chart
-import './index.scss'
-import { createParallelChart } from './parallel'
-
-// Funzione per caricare i dati e inizializzare il grafico
-function initializeParallelChart () {
-  // Carica i dati dal file CSV
-  // d3.csv(dataPath).then(data => {
-  //   // Chiama la funzione per creare il Parallel Coordinates Chart con i dati caricati
-  //   // createParallelChart(data)
-  //   console.log(Object.keys(data))
-  // }).catch(error => {
-  //   console.error('Errore nel caricamento dei dati:', error)
-  // })
-  createParallelChart()
-}
-
-// Chiamata alla funzione per inizializzare il grafico
-initializeParallelChart()
+console.log('Hello, world!')
